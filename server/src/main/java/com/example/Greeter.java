bcada39ae7370eed4d8fb4ce3ef030dbbc5b3bb9
babbgoud <sandeepb3245@gmail.com>
2020-06-23 14:55:09 +0530

    fixed checkstyle warinings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
   /**
    * This is a final method.
    */
-/**{@inheritdoc} */
+/**{@inheritDoc} */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
