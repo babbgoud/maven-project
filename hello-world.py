58f4a891bbbde5d5a3395e8fcdb970cb9aeec89d
babbgoud <sandeepb3245@gmail.com>
2020-07-07 23:50:32 +0530

    fixed script issues

--- hello-world.py
+++ hello-world.py
@@ -18,9 +18,8 @@ def printBox(symbol, width, height):
     print(symbol * (width)) # prints "first line as *********"
     for i in range(height - 2):
         print(symbol + (' ' * (width - 2) ) + symbol) # prints next lines as "*   *"
-        print(symbol * width) # prints "first last line as *********"
-        
-   return ''
+    print(symbol * width) # prints "first last line as *********"
+    return ''
         
  
 print("Good Morning\n Welcome to Python script ..\n")
